c73478369d287dced0a60120b13228e00ea87606
Samson Maconi <nivlek_000@yahoo.com>
2018-03-11 21:09:13 +0100

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+const APP_TITLE = 'WeConnect';
+const JUMBOTRON_MAIN_TEXT = 'Connecting People';
+const JUMBOTRON_SUB_TEXT = 'What are you waiting for?';
+const COPYRIGHT = 'Chucks Amaechi';
+
+
+//document.getElementById("navBarAppTitle").innerHTML = APP_TITLE;
+//document.getElementById("footerAppTitle").innerHTML = APP_TITLE;
+document.getElementById("jumbotronMainText").innerHTML = JUMBOTRON_MAIN_TEXT;
+document.getElementById("jumbotronSubText").innerHTML = JUMBOTRON_SUB_TEXT;
+document.getElementById("copyright").innerHTML = COPYRIGHT;
